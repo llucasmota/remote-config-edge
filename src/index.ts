ab43cca9bba21e8c8f86a2708f0ec1319d33af52
llucasmota <lucas.o.mota@gmail.com>
2026-09-22 06:30:38 -0300

    feat(index.ts): It's been created an index to serve a inject json

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,15 @@
+import { validateConfig } from "./validator/validator.js";
+
 /**
  * Remote Config Edge Platform
  * High-performance edge-distributed configuration engine for mobile banking.
  */
 export const VERSION = '0.1.0';
+const featureSchema = {
+  minVersion: 1.0,
+  enabled: true,
+  path: "/app-mobile",
+  userType: ["PF"]
+}
+
+validateConfig("featureSchema", featureSchema)
