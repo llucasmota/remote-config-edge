388c6eff01238892fae54c8d175fd26758cc054f
llucasmota <lucas.o.mota@gmail.com>
2026-09-23 07:11:46 -0300

    test(app.test.ts): It's been created new tests for app.ts

--- src/app.test.ts
+++ src/app.test.ts
@@ -12,6 +12,7 @@ describe('Fastify Application Setup', () => {
     console.log('🤖 app.test.ts 🤖')
 
     expect(response.statusCode).toBe(200);
+
     const body = JSON.parse(response.body);
     expect(body.status).toBe('ok')
     expect(body).toHaveProperty('timestamp')
